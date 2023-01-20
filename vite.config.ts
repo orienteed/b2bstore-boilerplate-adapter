177ce673d2fc227d90ab2a98864ec7b6851e44a4
jalvarez00 <jalvarez@orienteed.com>
2023-01-20 10:08:39 +0100

    Modify build options

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { resolve } from 'path';
 export default defineConfig({
   build: {
     lib: {
-      entry: resolve('src', 'index.ts'),
+      entry: resolve(__dirname, 'src/index.ts'),
       name: 'b2bstore-boilerplate-adapter',
       fileName: 'b2bstore-boilerplate-adapter',
     },
